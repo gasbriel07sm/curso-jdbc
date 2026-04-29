4ce9daa45d0c37acf1e118c16a50ebcf13ab4354
gabrielmiranda07sm <gasmiranda07@gmail.com>
2026-04-29 11:36:00 -0300

    Curso JDBC - Inserindo Dados

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,36 +1,65 @@
 package application;
-
 import db.DB;
+import db.DbException;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Program {
     public static void main(String[] args) {
 
-        Connection conn = DB.getConnection();
-        Statement st = null;
-        ResultSet rs = null;
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        Connection conn = null;
+        PreparedStatement st = null;
 
         try {
             conn = DB.getConnection();
 
-            st = conn.createStatement();
-            rs = st.executeQuery("select * from department");
+            st = conn.prepareStatement(
+                    "INSERT INTO seller " +
+                        "(Name, Email, BirthDate, BaseSalary, DepartmentId)" +
+                        "VALUES " +
+                        "(?, ?, ?, ? ,?)",
+                    Statement.RETURN_GENERATED_KEYS);
 
-            while(rs.next()) {
-                System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+            st.setString(1, "Carl Purple");
+            st.setString(2, "carl@gmail.com");
+            st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
+            st.setDouble(4, 3000.00);
+            st.setInt(5, 4);
+
+            st = conn.prepareStatement(
+                    "INSERT INTO department " +
+                        "(Name) " +
+                        "VALUES " +
+                        "('D1'),('D2')",
+                        Statement.RETURN_GENERATED_KEYS
+            );
+
+            int rowsAffected = st.executeUpdate();
+
+            if (rowsAffected > 0) {
+                ResultSet rs = st.getGeneratedKeys();
+                while (rs.next()) {
+                    int id = rs.getInt(1);
+                    System.out.println("Done! ID = " + id);
+                }
+            } else {
+                System.out.println("No rows affected!");
             }
         }
         catch (SQLException e) {
-            e.printStackTrace();
+            throw new DbException(e.getMessage());
+        }
+        catch (ParseException e) {
+            throw new RuntimeException(e);
         }
         finally {
-            DB.closeResultSet(rs);
             DB.closeStatement(st);
             DB.closeConnection();
         }
+
     }
 }
