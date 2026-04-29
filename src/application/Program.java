3d639b11c51cdc81af0f96e9ed4c262bd4108e28
gabrielmiranda07sm <gasmiranda07@gmail.com>
2026-04-29 19:25:14 -0300

    Curso JDBC - Deletando Dados

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 import db.DB;
 import db.DbException;
+import db.DbIntegrityException;
 
 import java.sql.*;
 
@@ -15,21 +16,20 @@ public class Program {
             conn = DB.getConnection();
 
             st = conn.prepareStatement(
-                    "UPDATE seller "
-                    + "SET BaseSalary = BaseSalary + ? "
+                    "DELETE FROM department "
                     + "WHERE "
-                    + "(DepartmentId = ?)"
+                    + "Id = ?"
             );
 
-            st.setDouble(1, 200);
-            st.setInt(2, 2);
+            st.setInt(1, 2);
 
             int rowsAffected = st.executeUpdate();
             System.out.println("Done! Rows addected: " + rowsAffected);
         }
         catch (SQLException e) {
-            e.printStackTrace();
+            throw new DbIntegrityException(e.getMessage());
         }
+
         finally {
             DB.closeStatement(st);
             DB.closeConnection();
