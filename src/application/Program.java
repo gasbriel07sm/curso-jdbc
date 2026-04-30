6228686618226b267d350c94ac91fef55ba2e66a
gabrielmiranda07sm <gasmiranda07@gmail.com>
2026-04-30 12:48:42 -0300

    Projeto DAO JDBC - findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,13 +13,17 @@ public class Program {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        System.out.println("=== TES 1: seller findById ===");
+        System.out.println("=== TEST 1: seller findById ===");
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
 
-        System.out.println("\n=== TES 1: seller findByDepartment ===");
+        System.out.println("\n=== TEST 2: seller findByDepartment ===");
         Department department = new Department(2, null);
         List<Seller> list = sellerDao.findByDepartment(department);
+        list.forEach(System.out::println);
+
+        System.out.println("\n=== TEST 3: seller findAll ===");
+        list = sellerDao.findAll();
         list.forEach(System.out::println);
 
     }
