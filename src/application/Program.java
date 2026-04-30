2be1c5e8fc2a97b05396bbe5007e1a92ccd0f0bc
gabrielmiranda07sm <gasmiranda07@gmail.com>
2026-04-30 12:45:48 -0300

    Projeto DAO JDBC - findByDepartment implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,6 +6,7 @@ import model.entities.Department;
 import model.entities.Seller;
 
 import java.util.Date;
+import java.util.List;
 
 public class Program {
     public static void main(String[] args) {
@@ -16,6 +17,10 @@ public class Program {
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
 
+        System.out.println("\n=== TES 1: seller findByDepartment ===");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+        list.forEach(System.out::println);
 
     }
 }
